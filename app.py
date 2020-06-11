e7e2851a214bc4760aa7e2fbf73bd1023ccb9b33
bartek <bartosz.piotr.baranowski@gmail.com>
2020-06-11 19:25:23 +0200

    Add swagger documentation

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flasgger import Swagger, swag_from
 
 app = Flask(__name__)
 api = Api(app)
+swagger = Swagger(app)
+
 
 
 class SortBoardsingCards(Resource):
@@ -12,6 +15,7 @@ class SortBoardsingCards(Resource):
     """
     # POST method
     # curl http://localhost:5000/ -d "{"city":"Wwa", "sex":"Male"}"
+    @swag_from('tripsort.yml')
     def post(self):
 
         json_data = request.get_json(force=True)
